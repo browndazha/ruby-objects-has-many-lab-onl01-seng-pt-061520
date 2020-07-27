fe4e466c8aec2ca6b191bef42c73fd73640f52ca
Dazha Brown <dajahbrown@hotmail.com>
2020-07-27 02:46:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,25 @@
-class Artist
-  attr_accessor :name
+class Artist 
 
-  @@song_count = 0
+  attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
-    @songs = []
-  end
-
-  def self.song_count
-    @@song_count
+    @@all << self
   end
 
   def songs
-    @songs << song_name
+    Song.all.select {|song| song.artist == self}
   end
 
-  def add_song(song_name)
-    @songs << song_name
-    song_name.artist = self
-    @@song_count += 1
+  def genres
+    songs.map {|song| song.genre}
   end
 
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+  def self.all
+    @@all
+  end
+
+  def new_song(song, genre)
+    Song.new(name, self, genre)
   end
